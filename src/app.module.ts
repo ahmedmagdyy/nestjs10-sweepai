de097f846aaebeaff61704627b2a6f0f776a8b32
ahmedmagdyy <ahmedmagdy2016@gmail.com>
2023-08-11 18:52:53 +0300

    fix swc compiler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { DatabaseConnection } from './path/to/database.connection';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UserModule, MongooseModule.forRootAsync({
-    useClass: DatabaseConnection,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_CONNECTION_STRING),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
